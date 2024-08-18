dc912bf3a77b722f369370e0ead61e82fe43cdd0
HungParfait <81432965+HungParfait@users.noreply.github.com>
2024-08-18 14:53:25 +0700

    Update pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,19 +1,66 @@
-# #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+import datetime
 
 
-# Start the browser and login with standard_user
-def login (user, password):
-    print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return ts + '\t'
+
+
+def start_browser():
+    options = ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument("--headless")
+    return webdriver.Chrome(options=options)
+
+
+def login(driver, user, password, file):
+    print(timestamp() + 'Starting the browser...', file=file)
     driver.get('https://www.saucedemo.com/')
+    driver.find_element(By.ID, "user-name").send_keys(user)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
+    product_label = driver.find_element(By.CSS_SELECTOR, "span[class='title']").text
+    assert "Products" in product_label
+    print(timestamp() + f'Login with username {user} and password {password} successfully.', file=file)
 
-login('standard_user', 'secret_sauce')
 
-# ToDo: Add more functional UI tests as per your requirements. 
+def add_to_cart(driver, item_count, file):
+    for i in range(item_count):
+        element = f"a[id='item_{i}_title_link']"
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()
+        count = driver.find_element(By.CSS_SELECTOR, "span[class='shopping_cart_badge']").text
+        assert int(count) == (i + 1)
+        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        print(timestamp() + f"{product} added to shopping cart.", file=file)
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+    print(timestamp() + f"{item_count} items are all added to the shopping cart successfully.", file=file)
+
+
+def remove_from_cart(driver, item_count, file):
+    for i in range(item_count):
+        element = f"a[id='item_{i}_title_link']"
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
+        if i < N_ITEMS - 1:
+            count = driver.find_element(By.CSS_SELECTOR, "span[class='shopping_cart_badge']").text
+            assert int(count) == N_ITEMS - (i + 1)
+        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name large_size']").text
+        print(timestamp() + f"{product} removed from the shopping cart.", file=file)
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
+    print(timestamp() + f"{item_count} items are all removed from the shopping cart successfully.", file=file)
+
+
+if __name__ == "__main__":
+    N_ITEMS = 6
+    TEST_USERNAME = 'standard_user'
+    TEST_PASSWORD = 'secret_sauce'
+    with open('selenium.log', 'a') as sample:
+        xdriver = start_browser()
+        login(xdriver, TEST_USERNAME, TEST_PASSWORD, sample)
+        add_to_cart(xdriver, N_ITEMS, sample)
+        remove_from_cart(xdriver, N_ITEMS, sample)
+        print(timestamp() + 'Selenium tests are all successfully completed!', file=sample)
