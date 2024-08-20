20c86c9b2bb5842b3587d6ee93b5f380e44b7c19
HungParfait <81432965+HungParfait@users.noreply.github.com>
2024-08-20 16:09:48 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,32 +1,33 @@
 resource "azurerm_network_interface" "NI" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "NI"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = var.subnet_id_test
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "VM" {
-  name                  = ""
-  location              = ""
-  resource_group_name   = ""
+  name                  = "Vm2"
+  location              = var.location
+  resource_group_name   = var.resource_group
   size                  = "Standard_DS2_v2"
   admin_username        = var.admin_username
   admin_password        = var.admin_password
-  network_interface_ids = []
+  network_interface_ids = [azurerm_network_interface.NI.id]
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  source_image_id = var.source_image_id
+  # source_image_reference {
+  #   publisher = "Canonical"
+  #   offer     = "UbuntuServer"
+  #   sku       = "18.04-LTS"
+  #   version   = "latest"
+  # }
 }
