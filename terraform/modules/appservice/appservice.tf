32e63c51e2a11e14b872a955f011699c61a636f2
HungParfait <81432965+HungParfait@users.noreply.github.com>
2024-08-21 12:19:53 +0700

    update pipeline

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -6,21 +6,14 @@ resource "azurerm_service_plan" "test" {
   sku_name            = "F1"
 }
 
-resource "azurerm_windows_web_app" "test" {
+resource "azurerm_app_service" "test" {
   name                = var.application_name
   location            = var.location
   resource_group_name = var.resource_group
-  service_plan_id     = azurerm_service_plan.test.id
-
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
-  site_config {
-    always_on = false
-    application_stack {
-      current_stack = "dotnetcore"
-      dotnet_core_version = "v4.0"
-    }
-  }
+
+  app_service_plan_id = azurerm_service_plan.test.id
 }
