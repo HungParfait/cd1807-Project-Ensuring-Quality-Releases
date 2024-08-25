17a2ebca5892be07d173256455c7e455b148971b
HungParfait <81432965+HungParfait@users.noreply.github.com>
2024-08-25 22:43:12 +0700

    update pipeline

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_virtual_machine" "VM" {
   source_image_reference {
      publisher = "Canonical"
      offer     = "UbuntuServer"
-     sku       = "18.04-LTS"
+     sku       = "22.04-LTS"
      version   = "latest"
   }
 }
